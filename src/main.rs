782d5a028fcaa2d117a0f7b4fd680780e3c8a78b
Bak Gergely János <16374863+weyh@users.noreply.github.com>
2022-01-28 19:38:33 +0100

    Moved clap arg parse to separate file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-use clap::{App, Arg};
-
 #[cfg(test)]
 mod tests;
 
+mod args;
 mod reqs_check;
 mod session_core;
 mod ui;
@@ -20,40 +19,7 @@ fn main() {
         return;
     }
 
-    let matches = App::new(env!("CARGO_CRATE_NAME"))
-        .version(env!("CARGO_PKG_VERSION"))
-        .arg(
-            Arg::new("user_config")
-                .short('u')
-                .long("user-config")
-                .help("Path to user config file")
-                .value_name("USER_CONFIG")
-                .required(true),
-        )
-        .arg(
-            Arg::new("list")
-                .short('l')
-                .long("list")
-                .conflicts_with_all(&["connect", "remove"])
-                .help("List all sessions"),
-        )
-        .arg(
-            Arg::new("connect")
-                .short('c')
-                .long("connect")
-                .value_name("SESSION_INDEX")
-                .conflicts_with_all(&["list", "remove"])
-                .help("Connect to session by index"),
-        )
-        .arg(
-            Arg::new("remove")
-                .short('r')
-                .long("remove")
-                .value_name("SESSION_GROUP_NAME")
-                .conflicts_with_all(&["list", "connect"])
-                .help("Remove session group by name"),
-        )
-        .get_matches();
+    let matches = args::get_args();
 
     let user_config = matches.value_of("user_config").unwrap();
     if user_config != "" {
