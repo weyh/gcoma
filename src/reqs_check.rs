1f53fa7fcececf7af8790a2eaee10ec3be0bd6df
Bak Gergely János <16374863+weyh@users.noreply.github.com>
2022-01-29 11:20:36 +0100

    v1.3.1
    Fix requirements check for win

--- src/reqs_check.rs
+++ src/reqs_check.rs
@@ -2,14 +2,31 @@ use std::env;
 use std::fs;
 
 #[cfg(target_family = "windows")]
-const SPLITTER: &str = ";";
-
-#[cfg(target_family = "unix")]
-const SPLITTER: &str = ":";
-
 pub fn is_in_env(prog_name: &str) -> bool {
     if let Ok(path) = env::var("PATH") {
-        for p in path.split(SPLITTER) {
+        for p in path.split(";") {
+            // assuming that the binary has an extension
+            for ext in env::var("PATHEXT").unwrap_or("exe".to_string()).split(";") {
+                let p_str = format!("{}{}{}", p, prog_name, ext);
+                if fs::metadata(p_str).is_ok() {
+                    return true;
+                }
+
+                let p_str_slash = format!("{}\\{}{}", p, prog_name, ext);
+                if fs::metadata(p_str_slash).is_ok() {
+                    return true;
+                }
+            }
+        }
+    }
+
+    false
+}
+
+#[cfg(target_family = "unix")]
+pub fn is_in_env(prog_name: &str) -> bool {
+    if let Ok(path) = env::var("PATH") {
+        for p in path.split(":") {
             let p_str = format!("{}/{}", p, prog_name);
             if fs::metadata(p_str).is_ok() {
                 return true;
