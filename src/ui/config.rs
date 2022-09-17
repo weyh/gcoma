20ce9c1d145c6269682c6e8fe6298b01e4156177
Bak Gergely János <16374863+weyh@users.noreply.github.com>
2022-09-17 19:33:39 +0200

    Swap ansi_term to yansi

--- src/ui/config.rs
+++ src/ui/config.rs
@@ -1,40 +1,40 @@
-use ansi_term::Colour::RGB;
 use serde::{Deserialize, Serialize};
+use yansi::Color::RGB;
 
 use crate::session_core::session_group::SessionGroup;
 
 #[derive(Serialize, Deserialize)]
-pub struct Color {
+pub struct MyColor {
     r: u8,
     g: u8,
     b: u8,
 }
 
-macro_rules! color_to_acolour {
+macro_rules! mycolor_to_yansicolour {
     ($color:expr) => {
         RGB($color.r as u8, $color.g as u8, $color.b as u8)
     };
 }
 
-const DEFAULT_PRINARY_COLOR: Color = Color {
+const DEFAULT_PRINARY_COLOR: MyColor = MyColor {
     r: 248,
     g: 248,
     b: 242,
 };
 
-const DEFAULT_SUCCESS_COLOR: Color = Color {
+const DEFAULT_SUCCESS_COLOR: MyColor = MyColor {
     r: 80,
     g: 250,
     b: 123,
 };
 
-const DEFAULT_HIGHLIGHT_COLOR: Color = Color {
+const DEFAULT_HIGHLIGHT_COLOR: MyColor = MyColor {
     r: 241,
     g: 250,
     b: 140,
 };
 
-const DEFAULT_ERROR_COLOR: Color = Color {
+const DEFAULT_ERROR_COLOR: MyColor = MyColor {
     r: 241,
     g: 250,
     b: 140,
@@ -42,27 +42,27 @@ const DEFAULT_ERROR_COLOR: Color = Color {
 
 #[derive(Serialize, Deserialize)]
 pub struct UIColors {
-    pub primary_color: Color,
-    pub success_color: Color,
-    pub error_color: Color,
-    pub highlight_color: Color,
+    pub primary_color: MyColor,
+    pub success_color: MyColor,
+    pub error_color: MyColor,
+    pub highlight_color: MyColor,
 }
 
 impl UIColors {
-    pub fn primary(&self) -> ansi_term::Colour {
-        color_to_acolour!(&self.primary_color)
+    pub fn primary(&self) -> yansi::Color {
+        mycolor_to_yansicolour!(&self.primary_color)
     }
 
-    pub fn success(&self) -> ansi_term::Colour {
-        color_to_acolour!(&self.success_color)
+    pub fn success(&self) -> yansi::Color {
+        mycolor_to_yansicolour!(&self.success_color)
     }
 
-    pub fn error(&self) -> ansi_term::Colour {
-        color_to_acolour!(&self.error_color)
+    pub fn error(&self) -> yansi::Color {
+        mycolor_to_yansicolour!(&self.error_color)
     }
 
-    pub fn highlight(&self) -> ansi_term::Colour {
-        color_to_acolour!(&self.highlight_color)
+    pub fn highlight(&self) -> yansi::Color {
+        mycolor_to_yansicolour!(&self.highlight_color)
     }
 }
 
